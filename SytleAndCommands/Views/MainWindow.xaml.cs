0606287dab12b4e0c6a603cb44e000a4dba681d7
Omer <elvin@mail.ru>
2022-12-01 11:37:23 +0400

    done

--- SytleAndCommands/Views/MainWindow.xaml.cs
+++ SytleAndCommands/Views/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,29 +21,100 @@ namespace SytleAndCommands
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,INotifyPropertyChanged
     {
         public class DataObject
         {
             public int TheValue { get; set; }
         }
+
+
+
+        private int age;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; OnPropertyChanged(); }
+        }
+
         public MessageCommand MessageCommand { get; set; }
         public MessageCommand OpenNewWindow { get; set; }
+
+        public RelayCommand ShowDataCommand { get; set; }
+        public RelayCommand UsernameEnterCommand { get; set; }
+        public RelayCommand PasswordEnterCommand { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             //DataContext = new DataObject();
             this.DataContext = this;
 
-            MessageCommand = new MessageCommand(() =>
+            //MessageCommand = new MessageCommand(() =>
+            //{
+            //    MessageBox.Show("I clicked to Send Message");
+            //});
+
+            //OpenNewWindow = new MessageCommand(() =>
+            //{
+            //    MessageBox.Show("I opened New Window");
+            //});
+
+            //ShowDataCommand = new RelayCommand((o) =>
+            //{
+            //    MessageBox.Show("Test");
+            //}, (o) =>
+            //{
+            //    return Age >= 18;
+            //});
+
+
+            //ShowDataCommand = new RelayCommand((o) =>
+            //{
+            //    MessageBox.Show("Test");
+            //});
+
+
+
+
+            UsernameEnterCommand = new RelayCommand((o) =>
             {
-                MessageBox.Show("I clicked to Send Message");
+                var myStackPanel = o as StackPanel;
+                var usernameTxtB = myStackPanel.Children[0] as TextBox;
+                var button = myStackPanel.Children[2] as Button;
+                if (usernameTxtB.Text.Trim() == String.Empty)
+                {
+                    usernameTxtB.BorderBrush = Brushes.Red;
+                    usernameTxtB.BorderThickness = new Thickness(6);
+                    button.Focus();
+                }
+                else
+                {
+                    usernameTxtB.BorderBrush = Brushes.SpringGreen;
+                    usernameTxtB.BorderThickness = new Thickness(2);
+
+                    var passwordTxtb=myStackPanel.Children[1] as TextBox;
+                    passwordTxtb.Focus();
+                }
             });
 
-            OpenNewWindow = new MessageCommand(() =>
+            PasswordEnterCommand = new RelayCommand((o) =>
             {
-                MessageBox.Show("I opened New Window");
+                var myStackPanel = o as StackPanel;
+                var button = myStackPanel.Children[2] as Button;
+                button.Focus();
             });
+            
 
         }
     }
